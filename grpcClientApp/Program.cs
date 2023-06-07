511a4c93a5fadd26743dfd1aa463ee15c0d016e1
Mukasa Mukasa <mutebir2@gmail.com>
2023-06-07 17:51:19 +0100

    firstcommit
    GPRC project

---
+++ grpcClientApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using Grpc.Net.Client;
+using GrpcService1;
+using System;
+using System.Threading.Tasks;
+
+namespace grpcClientApp
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var input = new HelloRequest { StudentDetails = "Stuart Mukasa Student at NCI HDIP", Age="90" };
+            var channel = GrpcChannel.ForAddress("http://localhost:5187");
+            var client = new Greeter.GreeterClient(channel);
+            var reply = await client.SayHelloAsync(input);
+            Console.WriteLine(reply.Message);
+            Console.ReadLine();
+        }
+    }
+}
